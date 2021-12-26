127eb3fcd6db6fc0760ca97fac8c9a4694ce3f9b
Kozachenko Oleksandr <o.kozachenko@binzel.local>
2021-12-26 20:40:29 +0200

    First commit - create

---
+++ main.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+window = tk.Tk()
+ROW = 5
+COLUMN = 7
+buttons = []
+
+for i in range(ROW):
+    temp = []
+    for j in range(COLUMN):
+        btn = tk.Button(window, width=3, font='Calibri 15 bold')
+        temp.append(btn)
+    buttons.append(temp)
+
+for row_btn in buttons:
+    print(row_btn)
+
+for i in range(ROW):
+    for j in range(COLUMN):
+        btn = buttons[i][j]
+        btn.grid(row=i, column=j)
+
+
+window.mainloop()
